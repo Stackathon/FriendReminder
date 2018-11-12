89dbd5e014b80f69d0539cfa7a6fabb09ec5e574
Ay-Nur Najm <aynajm@gmail.com>
2018-11-12 09:32:22 -0500

    feature posted data

--- src/Results.js
+++ src/Results.js
@@ -2,19 +2,19 @@ import React from 'react';
 import axios from 'axios'
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
-// const friends = [
-//   {name: 'Michael Jackson', phone: '123', response: 'Yes'},
-//   {name: 'Ay-Nur', phone: '321', response: 'No'},
-//   {name: 'Connie', phone: '111', response: 'Yes'},
-//   {name: 'Beyonce', phone: '222', response: 'Maybe'},
-//   {name: 'Rihanna', phone: '333', response: 'Yes'},
-// ];
+const friends = [
+  {name: 'Michael Jackson', phone: '123', response: 'Yes'},
+  {name: 'Ay-Nur', phone: '321', response: 'No'},
+  {name: 'Connie', phone: '111', response: 'Yes'},
+  {name: 'Beyonce', phone: '222', response: 'Maybe'},
+  {name: 'Rihanna', phone: '333', response: 'Yes'},
+];
 
 export default class SelectFilterWithDefaultValue extends React.Component {
   constructor (props) {
     super(props) 
     this.state = {
-        responses: []
+        responses: [],
     }
   }
   async componentDidMount () {
@@ -26,16 +26,26 @@ export default class SelectFilterWithDefaultValue extends React.Component {
   }
   render() {
     const responses = this.state.responses
+    
     return (
-      <BootstrapTable data={ responses } bordered >
-          <TableHeaderColumn dataField='content' isKey={ true }>Response</TableHeaderColumn>
-          {/* <TableHeaderColumn dataField='content'>Phone</TableHeaderColumn>      */}
-          {/* <TableHeaderColumn dataField='content'>Phone</TableHeaderColumn>      */}
-          {/* <TableHeaderColumn 
-            dataField='response' filter={ { type: 'TextFilter', defaultValue: ' ' } }>
-            Friend Responses
-          </TableHeaderColumn> */}
-      </BootstrapTable>
+      <div>
+        <h2>Expectations...</h2>
+          <BootstrapTable data={ friends } bordered >
+            <TableHeaderColumn dataField='name' isKey={ true }>Name</TableHeaderColumn>
+            <TableHeaderColumn dataField='phone'>Phone</TableHeaderColumn>     
+            <TableHeaderColumn dataField='response' filter={ { type: 'TextFilter', defaultValue: ' ' } }>Response</TableHeaderColumn>     
+          </BootstrapTable>
+          <h2>Reality :( ...</h2>
+          <BootstrapTable data={ responses } bordered >
+              <TableHeaderColumn dataField='content' isKey={ true }>Response</TableHeaderColumn>
+              {/* <TableHeaderColumn dataField='content'>Phone</TableHeaderColumn>      */}
+              {/* <TableHeaderColumn dataField='content'>Phone</TableHeaderColumn>      */}
+              {/* <TableHeaderColumn 
+                dataField='response' filter={ { type: 'TextFilter', defaultValue: ' ' } }>
+                Friend Responses
+              </TableHeaderColumn> */}
+          </BootstrapTable>
+      </div>
     );
   }
 }
